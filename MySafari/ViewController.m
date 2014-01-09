334b9abd9309e46dc4f6767c5bdbbdbeed28cdb8
McMurdo1 <matt@1423ventures.com>
2014-01-09 17:27:49 -0600

    Finalized all stretch goals. Nav bar disappears and reappears. Page title contains page title.

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -7,11 +7,23 @@
 //
 
 #import "ViewController.h"
+#include <math.h>
 
-@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate>
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate, UIScrollViewDelegate>
 {
     __weak IBOutlet UITextField *myURLTextField;
     __weak IBOutlet UIWebView *myWebView;
+    __weak IBOutlet UIButton *myBackButton;
+    
+    __weak IBOutlet UILabel *titleLabel;
+    __weak IBOutlet UINavigationItem *myNavigationTitle;
+    __weak IBOutlet UIButton *myReloadButton;
+    __weak IBOutlet UIButton *myNewFeaturesButton;
+    __weak IBOutlet UIButton *myStopButton;
+    
+    __weak IBOutlet UIButton *myForwardButton;
+    
+    __weak IBOutlet UIView *myButtonView;
 }
 @end
 
@@ -21,15 +33,42 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    myWebView.scrollView.delegate = self;
 }
 
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField{
-    NSURL *userInputURL = [NSURL URLWithString:myURLTextField.text];
+    NSString *userInputURLString = myURLTextField.text;
+    BOOL hasHTTP = [[userInputURLString lowercaseString] hasPrefix:@"http://"];
+    
+    if (hasHTTP == NO) {
+       userInputURLString = [NSString stringWithFormat:@"http://%@", userInputURLString];
+        
+        
+        
+    }
+    
+    
+    NSURL *userInputURL = [NSURL URLWithString:userInputURLString];
     NSURLRequest *userURLRequest = [NSURLRequest requestWithURL:userInputURL];
     [myWebView loadRequest:userURLRequest];
+    [myURLTextField resignFirstResponder];
     return YES;
 }
+
+
+-(void)webViewDidStartLoad:(UIWebView *)webView {
+    
+    NSString *anyHTTP = myWebView.request.URL.absoluteString;
+    myURLTextField.text = anyHTTP;
+
+
+     NSString *pageTitle = [myWebView stringByEvaluatingJavaScriptFromString:@"document.title"];
+    
+   titleLabel.text  = pageTitle;
+
+}
+
 
 
 - (IBAction)onBackButtonPressed:(id)sender {
@@ -54,6 +93,43 @@
     
     [myWebView reload];
 }
+
+- (IBAction)onNewFeaturesButtonPressed:(id)sender {
+    
+    UIAlertView *newFeaturesAlert = [[UIAlertView alloc] initWithTitle:@"Coming Soon!" message:@"Check back to see our awesome new features" delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
+    [newFeaturesAlert show];
+}
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    NSString *scrollContentOffset = [NSString stringWithFormat:@"%f",scrollView.contentOffset.y ];
+    NSLog(scrollContentOffset);
+    if (scrollView.contentOffset.y > 0)
+    {
+        myURLTextField.alpha = 0;
+        myBackButton.alpha = 0;
+        myForwardButton.alpha = 0;
+        myNewFeaturesButton.alpha = 0;
+        myReloadButton.alpha = 0;
+        myStopButton.alpha = 0;
+        myButtonView.alpha = ( 1- (scrollView.contentOffset.y/100));
+        
+        
+    }
+    if (scrollView.contentOffset.y <= 0)
+    {
+        myURLTextField.alpha = 1;
+        myBackButton.alpha = 1;
+        myForwardButton.alpha = 1;
+        myNewFeaturesButton.alpha = 1;
+        myReloadButton.alpha = 1;
+        myStopButton.alpha = 1;
+        myButtonView.alpha = 1 ;
+    }
+    
+    
+}
+
 
 
 - (void)didReceiveMemoryWarning
